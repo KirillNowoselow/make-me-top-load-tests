73ffa78418d655e206a2379981cc04421f2548c4
xa4ipur4ik <98112009+KirillNowoselow@users.noreply.github.com>
2024-04-20 14:22:28 +0300

    Update HomeWorkServiceLoadTest.scala

--- src/test/scala/simulations/HomeWorkServiceLoadTest.scala
+++ src/test/scala/simulations/HomeWorkServiceLoadTest.scala
@@ -3,6 +3,7 @@ package simulations
 import io.gatling.http.Predef._
 import io.gatling.core.Predef._
 import scenarios.Simulation._
+
 class HomeWorkServiceLoadTest extends Simulation{
   val httpConf = http.baseUrl("http://10.254.1.192:8107/")
 
